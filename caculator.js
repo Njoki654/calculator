aa4860b409d2f6172de8122bfad075c8e6d49894
Njoki654 <annenjoki300@gmail.com>
2025-07-03 06:20:19 -0700

    caculator.js

--- caculator.js
+++ caculator.js
@@ -28,6 +28,10 @@ function calculate(){
     try{
         display.value =eval(display.value);
     } catch (error){
+function calculate(){
+    try{
+        display.value =eval(display.value);
+    } catch (error){
 display.value = "Error"
     }
 }
